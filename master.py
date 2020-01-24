c0bab58e566ac38282fb24c59ed99584ffe44e18
Sherry Shu <sherryshu@Sherrys-MacBook-Air.local>
2020-01-24 14:25:40 -0500

    game starts only after spacebar press

--- master.py
+++ master.py
@@ -37,20 +37,20 @@ human_times_hit = 0
 alien_times_hit = 0
 
 elapsed_time = 0
-time = False
+run_game = False
 
 if 19.00 < elapsed_time < 21.00 or 39.00 < elapsed_time < 41.00:
     draw_enemy_rate += 1
     draw_meteor_rate += 1
 
 for _ in range(draw_enemy_rate):
-    # generate random x and y values
+# generate random x and y values
     x_human = random.randrange(WIDTH/2 + 75, WIDTH - 75)
     y_human = random.randrange(HEIGHT, HEIGHT * 2)
     x_alien = random.randrange(75, WIDTH/2 - 75)
     y_alien = random.randrange(HEIGHT, HEIGHT * 2)
 
-    # append the x and y values to the appropriate list
+# append the x and y values to the appropriate list
     human_x_positions.append(x_human)
     human_y_positions.append(y_human)
     alien_x_positions.append(x_alien)
@@ -119,8 +119,6 @@ left_pressed_alien = False
 right_pressed_alien = False
 fire_laser_alien = False
 
-run_game = False
-
 main_screen_alien_x = 10
 main_screen_alien_y = 10
 main_screen_human_x = 10
@@ -140,111 +138,111 @@ start_time = 0
 
 
 def update(delta_time):
-    global elapsed_time, draw_enemy_rate, draw_meteor_rate, game_speed, counter, start_time
+    global elapsed_time, draw_enemy_rate, draw_meteor_rate, game_speed, counter, start_time, run_game
 
     counter += delta_time
 
-    if time == True:
+    if run_game == True:
         elapsed_time += delta_time
         elapsed_time = round(elapsed_time, 2)
 
-    for index in range(len(human_y_positions)):
-        human_y_positions[index] -= game_speed
+        for index in range(len(human_y_positions)):
+            human_y_positions[index] -= game_speed
 
-        if human_y_positions[index] < 0:
-            human_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
-            human_x_positions[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
+            if human_y_positions[index] < 0:
+                human_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                human_x_positions[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
 
-    for index in range(len(alien_y_positions)):
-        alien_y_positions[index] -= game_speed
+        for index in range(len(alien_y_positions)):
+            alien_y_positions[index] -= game_speed
 
-        if alien_y_positions[index] < 0:
-            alien_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
-            alien_x_positions[index] = random.randrange(75, WIDTH/2 - 75)
+            if alien_y_positions[index] < 0:
+                alien_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                alien_x_positions[index] = random.randrange(75, WIDTH/2 - 75)
 
-    for index in range(len(meteor_y_positions)):
-        meteor_y_positions[index] -= game_speed
+        for index in range(len(meteor_y_positions)):
+            meteor_y_positions[index] -= game_speed
 
-        if meteor_y_positions[index] < 0:
-            meteor_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
-            meteor_x_positions[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
+            if meteor_y_positions[index] < 0:
+                meteor_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                meteor_x_positions[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
 
-    for index in range(len(alien_meteor_y_positions)):
-        alien_meteor_y_positions[index] -= game_speed
+        for index in range(len(alien_meteor_y_positions)):
+            alien_meteor_y_positions[index] -= game_speed
 
-        if alien_meteor_y_positions[index] < 0:
-            alien_meteor_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
-            alien_meteor_x_positions[index] = random.randrange(75, WIDTH/2 - 75)
+            if alien_meteor_y_positions[index] < 0:
+                alien_meteor_y_positions[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                alien_meteor_x_positions[index] = random.randrange(75, WIDTH/2 - 75)
 
-    if elapsed_time > 60.00:
-        for index in range(len(human_y_positions_shooting_star)):
-            human_y_positions_shooting_star[index] -= game_speed
+        if elapsed_time > 60.00:
+            for index in range(len(human_y_positions_shooting_star)):
+                human_y_positions_shooting_star[index] -= game_speed
 
-            if human_y_positions_shooting_star[index] < 0:
-                if start_time == 0:
-                    start_time = counter
-                elif counter - start_time > 10:
-                    start_time = 0
-                    human_y_positions_shooting_star[index] = random.randrange(HEIGHT, HEIGHT + 50)
-                    human_x_positions_shooting_star[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
+                if human_y_positions_shooting_star[index] < 0:
+                    if start_time == 0:
+                        start_time = counter
+                    elif counter - start_time > 10:
+                        start_time = 0
+                        human_y_positions_shooting_star[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                        human_x_positions_shooting_star[index] = random.randrange(WIDTH/2 + 75, WIDTH - 75)
 
-        for index in range(len(alien_y_positions_shooting_star)):
-            alien_y_positions_shooting_star[index] -= game_speed
+            for index in range(len(alien_y_positions_shooting_star)):
+                alien_y_positions_shooting_star[index] -= game_speed
 
-            if alien_y_positions_shooting_star[index] < 0:
-                if start_time == 0:
-                    start_time = counter
-                elif counter - start_time > 10:
-                    start_time = 0
-                    alien_y_positions_shooting_star[index] = random.randrange(HEIGHT, HEIGHT + 50)
-                    alien_x_positions_shooting_star[index] = random.randrange(75, WIDTH/2 - 75)
+                if alien_y_positions_shooting_star[index] < 0:
+                    if start_time == 0:
+                        start_time = counter
+                    elif counter - start_time > 10:
+                        start_time = 0
+                        alien_y_positions_shooting_star[index] = random.randrange(HEIGHT, HEIGHT + 50)
+                        alien_x_positions_shooting_star[index] = random.randrange(75, WIDTH/2 - 75)
 
-    if elapsed_time == 20.00 or elapsed_time == 40.00:
-        game_speed += 0.8
-    elif elapsed_time > 60:
-        game_speed = 4.9
+        if elapsed_time == 20.00 or elapsed_time == 40.00:
+            game_speed += 0.8
+        elif elapsed_time > 60:
+            game_speed = 4.9
 
-    global human_hit, alien_hit
+        global human_hit, alien_hit
 
-    removed_human_lasers = 0
-    for index in range(len(human_y_positions_laser)):
-        if human_y_positions_laser[index - removed_human_lasers] < 765:
-            human_y_positions_laser[index - removed_human_lasers] += 30
-        else:
-            del human_x_positions_laser[index - removed_human_lasers]
-            del human_y_positions_laser[index - removed_human_lasers]
-            removed_human_lasers += 1
+        removed_human_lasers = 0
+        for index in range(len(human_y_positions_laser)):
+            if human_y_positions_laser[index - removed_human_lasers] < 765:
+                human_y_positions_laser[index - removed_human_lasers] += 30
+            else:
+                del human_x_positions_laser[index - removed_human_lasers]
+                del human_y_positions_laser[index - removed_human_lasers]
+                removed_human_lasers += 1
 
-    removed_alien_lasers = 0
-    for index in range(len(alien_y_positions_laser)):
-        if alien_y_positions_laser[index - removed_alien_lasers] < 765:
-            alien_y_positions_laser[index - removed_alien_lasers] += 30
-        else:
-            del alien_x_positions_laser[index - removed_alien_lasers]
-            del alien_y_positions_laser[index - removed_alien_lasers]
-            removed_alien_lasers += 1
+        removed_alien_lasers = 0
+        for index in range(len(alien_y_positions_laser)):
+            if alien_y_positions_laser[index - removed_alien_lasers] < 765:
+                alien_y_positions_laser[index - removed_alien_lasers] += 30
+            else:
+                del alien_x_positions_laser[index - removed_alien_lasers]
+                del alien_y_positions_laser[index - removed_alien_lasers]
+                removed_alien_lasers += 1
 
-    global left_pressed_human, left_pressed_alien, right_pressed_human, right_pressed_alien, spaceship_human_x, spaceship_alien_x
+        global left_pressed_human, left_pressed_alien, right_pressed_human, right_pressed_alien, spaceship_human_x, spaceship_alien_x
 
-    if left_pressed_human:
-        spaceship_human_x -= 5
-        if spaceship_human_x < 570:
-            spaceship_human_x = 570
+        if left_pressed_human:
+            spaceship_human_x -= 5
+            if spaceship_human_x < 570:
+                spaceship_human_x = 570
 
-    if left_pressed_alien:
-        spaceship_alien_x -= 5
-        if spaceship_alien_x < 60:
-            spaceship_alien_x = 60
+        if left_pressed_alien:
+            spaceship_alien_x -= 5
+            if spaceship_alien_x < 60:
+                spaceship_alien_x = 60
 
-    if right_pressed_human:
-        spaceship_human_x += 5
-        if spaceship_human_x > 940:
-            spaceship_human_x = 940
+        if right_pressed_human:
+            spaceship_human_x += 5
+            if spaceship_human_x > 940:
+                spaceship_human_x = 940
 
-    if right_pressed_alien:
-        spaceship_alien_x += 5
-        if spaceship_alien_x > 430:
-            spaceship_alien_x = 430
+        if right_pressed_alien:
+            spaceship_alien_x += 5
+            if spaceship_alien_x > 430:
+                spaceship_alien_x = 430
 
 
 def draw_main_screen(x, y):
@@ -312,9 +310,9 @@ def draw_end_screen(x, y):
                      arcade.color.BLACK, 65, font_name='GARA')
     arcade.draw_text("press space to restart", 350, 75, arcade.color.BLACK, 25)
 
-    if alien_points > human_points or alien_times_hit == 3:
+    if alien_times_hit >= 3 or alien_points > human_points:
         arcade.draw_text("The Humans Have Won Control!", 175, 400, arcade.color.BLACK, 40)
-    elif human_points > alien_points or human_times_hit == 3:
+    elif  human_times_hit >= 3 or human_points > alien_points:
         arcade.draw_text("The Aliens Have Won Control!", 200, 400, arcade.color.BLACK, 40)
     else:
         arcade.draw_text("It's a tie! Peace has been achieved.", 150, 400, arcade.color.BLACK, 40)
@@ -644,7 +642,7 @@ def on_draw():
     global spaceship_human_x, spaceship_human_y, spaceship_alien_x, spaceship_alien_y, run_game, time, elapsed_time, draw_enemy_rate, draw_meteor_rate
 
     arcade.start_render()
-    
+
     draw_main_screen(500, 375)
 
     if run_game == True:
@@ -693,7 +691,7 @@ def on_draw():
         
         elif 19 < elapsed_time < 21 or 39 < elapsed_time < 41 or 59 < elapsed_time < 61:
             arcade.draw_rectangle_filled(500, 375, 370, 100, arcade.color.BLACK)
-            arcade.draw_text("LEVEL UP!", 320, 342, arcade.color.WHITE, 65, font_name=('calibri'))
+            arcade.draw_text("LEVEL UP!", 320, 342, arcade.color.WHITE, 65)
 
         if 18.95 < elapsed_time < 19 or 38.95 < elapsed_time < 39 or 58.95 < elapsed_time < 59:
             arcade.sound.play_sound(level_up_sound)
@@ -739,11 +737,12 @@ def on_draw():
             alien_lives(112, 580)
             # arcade.sound.play_sound(lost_life_sound)
 
-    if human_times_hit >= 4:
+
+    if human_times_hit >= 3:
         run_game = False
         draw_end_screen(500, 375)
 
-    if alien_times_hit >= 4:
+    if alien_times_hit >= 3:
         run_game = False
         draw_end_screen(500, 375)
 
